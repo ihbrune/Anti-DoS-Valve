2ae1aff83ecbfdb368f5a8775f9e5cc23057ac14
Henning Brune <henning.brune@gmail.com>
2017-01-19 16:26:37 +0100

    Added license information

--- src/main/java/org/henbru/antidos/AntiDoSSlot.java
+++ src/main/java/org/henbru/antidos/AntiDoSSlot.java
@@ -8,6 +8,22 @@ import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
 /**
+ * Copyright 2017 Henning Brune
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ *************************
+ * 
  * This class implements the slots within the Anti-DoS Monitor. It manages a set
  * of counters that can, for example, represent the accesses of individual IP
  * addresses. The maximum number of managed counters is limited by the parameter
--- src/main/java/org/henbru/antidos/AntiDoSValve.java
+++ src/main/java/org/henbru/antidos/AntiDoSValve.java
@@ -15,6 +15,22 @@ import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
 /**
+ * Copyright 2017 Henning Brune
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ *************************
+ *
  * This is an implementation of a Tomcat Valve to accomplish an access rate
  * limitation for individual IP addresses. For this purpose, you can define how
  * many requests per time unit per IP are allowed. Any additional requests are
